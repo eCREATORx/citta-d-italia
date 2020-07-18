156ba06e05a5a1cc9cfb7d547b6edff3f59a858c
eCREATORx <dashka.tarasenko.2013@gmail.com>
2020-07-18 20:24:32 +0300

    Added api

--- bot.php
+++ bot.php
@@ -2,8 +2,11 @@
 
 include('vendor/autoload.php');
 use Telegram\Bot\Api;
+use GuzzleHttp\Client;
 
 $telegram = new Api('1016659380:AAFbs7DXrHGnd87S-4_-EF-7bXFeSmdV9bg');
+$key = 'AIzaSyAca6wkF2WEjAhKUxWG4j-puh4MixVnd9w';
+$cx = '007381751698148361103:jv6cuoyl1lu';
 $result = $telegram -> getWebhookUpdates();
 
 $text = $result["message"]["text"]; //Текст сообщения
@@ -13,24 +16,44 @@ $keyboard = [["Венеция"],["Милан"],["Рим"],["Флоренция"]
 
 if($text){
     if ($text == "/start") {
-        $reply = "Добро пожаловать в бота!";
+        $reply = "Добро пожаловать в бота! Выберите город для подписки";
         $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply, 'reply_markup' => $reply_markup ]);
     }elseif ($text == "/help") {
         $reply = "Информация с помощью.";
+        $reply_markup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $reply ]);
-    }elseif ($text == "Венеция") {
-        $url = "https://www.cruisetradenews.com/wp-content/uploads/2019/08/venice-canal-1.jpg";
-        $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Вененция" ]);
-    }elseif ($text == "Милан") {
-        $url = "https://34travel.me/media/upload/images/2016/march/milan_guide/new/IMG_6944.jpg";
-        $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Милан" ]);
-    }elseif ($text == "Рим") {
-        $url = "https://www.telegraph.co.uk/content/dam/Travel/Destinations/Europe/Italy/Rome/rome-sunset-tiber-river-overview-city-guide.jpg";
-        $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Рим" ]);
-    }elseif ($text == "Флоренция") {
-        $url = "https://specials-images.forbesimg.com/imageserve/852835136/960x0.jpg?fit=scale";
-        $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Флоренция" ]);
+    }elseif ($text == "Венеция" || "Милан" || "Рим" || "Флоренция") {
+        // Формируем запрос
+        $q = http_build_query(array(
+            'key' => $key,
+            'cx'  => $cx,
+            'searchType' => 'image',
+            'imgSize' => 'xxlarge',
+            'imgType' => 'photo',
+            'sort' => 'date:d:s',
+            'num' => 1,
+            'q' => $text // запрос для поиска
+        ));
+
+        // Инициализация клиента
+        $client = new Client(array(
+            'base_uri' => 'https://www.googleapis.com/customsearch/v1',
+            'query'    => $q,
+            'timeout'  => 3.0,
+            'debug'    => false,
+            'headers'  => array(
+                'Accept' => 'application/json'
+            ),
+        ));
+
+        // отправка запроса и получение результатов поиска
+        $response = $client->request('GET');
+        $results = json_decode($response->getBody()->getContents(), true);
+        $results = '"'.$results["items"][0]["link"].'"';
+
+        $url = $results;
+        $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Фотография города ".$text ]);
     }else{
         $reply = "По запросу \"<b>".$text."</b>\" ничего не найдено.";
         $telegram->sendMessage([ 'chat_id' => $chat_id, 'parse_mode'=> 'HTML', 'text' => $reply ]);
