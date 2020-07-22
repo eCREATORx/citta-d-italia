5e8137bd5daf5f3d49258638c569eb493a78387d
eCREATORx <dashka.tarasenko.2013@gmail.com>
2020-07-22 18:27:04 +0300

    Finished scheduler

--- sub.php
+++ sub.php
@@ -3,6 +3,7 @@
 include('vendor/autoload.php');
 require_once('vendor/thingengineer/mysqli-database-class/MysqliDb.php');
 require_once('vendor/thingengineer/mysqli-database-class/dbObject.php');
+require_once('api.php');
 use Telegram\Bot\Api;
 
 $telegram = new Api('1016659380:AAFbs7DXrHGnd87S-4_-EF-7bXFeSmdV9bg');
@@ -12,7 +13,8 @@ $db = new MysqliDb('us-cdbr-east-02.cleardb.com', 'b869ac278f05ad', '1dfb91f0', 
 $users = $db->getValue("subscriptions", "chat_id", null);
 foreach ($users as $chat_id) {
 	$cities = ($db->where('chat_id', $chat_id))->getValue("subscriptions", "city", 1);
-    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $cities ]);
+	$url = getUrl($cities);
+    $telegram->sendPhoto([ 'chat_id' => $chat_id, 'photo' => $url, 'caption' => "Фото по подписке ".$cities."."]);
 }
 
 ?>
