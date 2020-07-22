5e65436bd88caca7e3e08b8fd9c33133f4279e9a
eCREATORx <dashka.tarasenko.2013@gmail.com>
2020-07-22 18:09:21 +0300

    Update sub.php

--- sub.php
+++ sub.php
@@ -10,7 +10,9 @@ $telegram = new Api('1016659380:AAFbs7DXrHGnd87S-4_-EF-7bXFeSmdV9bg');
 $db = new MysqliDb('us-cdbr-east-02.cleardb.com', 'b869ac278f05ad', '1dfb91f0', 'heroku_f954956b083bef4');
 
 $users = $db->getValue("subscriptions", "chat_id", null);
-foreach ($users as $chat_id)
-    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => 'test schedule' ]);
+foreach ($users as $chat_id) {
+	$cities = ($db->where('chat_id', $chat_id))->getValue("subscriptions", "city", 1);
+    $telegram->sendMessage([ 'chat_id' => $chat_id, 'text' => $cities ]);
+}
 
 ?>
