1cef2936b571c0fff49926caf2d2565aecb71898
eCREATORx <dashka.tarasenko.2013@gmail.com>
2020-07-24 13:58:15 +0300

    Changed keyboard

--- bot.php
+++ bot.php
@@ -8,17 +8,19 @@ use Telegram\Bot\Api;
 $telegram = new Api('1016659380:AAFbs7DXrHGnd87S-4_-EF-7bXFeSmdV9bg');
 $result = $telegram->getWebhookUpdates();
 
-$cities = ['Бергамо', 'Венеция', 'Милан', 'Неаполь', 'Палермо', 'Рим', 'Турин', 'Флоренция'];
 $text = $result['message']['text']; // Текст сообщения
 $chatId = $result['message']['chat']['id']; // Уникальный идентификатор пользователя
-$keyboard = [['Бергамо', 'Венеция'], ['Милан', 'Неаполь'], ['Палермо', 'Рим'], ['Турин', 'Флоренция']]; //Клавиатура
+
+$cityList = ['Бергамо', 'Венеция', 'Милан', 'Неаполь', 'Палермо', 'Рим', 'Турин', 'Флоренция'];
+$cityChunks = array_chunk($cityList, 2);
+$keyboard = $cityChunks; //Клавиатура
 
 if($text){
     if ($text === '/start') {
         $reply = 'Добро пожаловать в бота! Выберите город для подписки.';
         $replyMarkup = $telegram->replyKeyboardMarkup([ 'keyboard' => $keyboard, 'resize_keyboard' => true, 'one_time_keyboard' => false ]);
         $telegram->sendMessage([ 'chat_id' => $chatId, 'text' => $reply, 'reply_markup' => $replyMarkup ]);
-    }elseif (in_array($text, $cities, true)) {
+    }elseif (in_array($text, $cityList, true)) {
         // Получение ссылки и отправка фото
         $url = getPhotoUrlFromQuery($text);
         $telegram->sendPhoto([ 'chat_id' => $chatId, 'photo' => $url, 'caption' => 'Фото по запросу '.$text.'.']);
